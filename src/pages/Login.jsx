3a426731ccf6a51094a235a384e9e6c7f8ae66a9
Nirmalya M. Thakur <nirmalyamallickthakur@gmail.com>
2024-08-01 11:45:40 +0530

    modified login and signup page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,14 +15,14 @@ function Login() {
   }
 
   return (
-    <div className="flex-col min-h-full justify-center px-6 py-12 lg:px-8">
+    <div className="flex flex-col h-screen justify-center px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
           className="mx-auto h-10 w-auto"
           src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
           alt="Your Company"
         />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Login to your account
         </h2>
       </div>
